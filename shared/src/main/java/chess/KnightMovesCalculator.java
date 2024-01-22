01d92936bad6140cacbe84cf6b8cfe904899d2d9
KaidenWilliams <kman16409@gmail.com>
2024-01-22 12:31:43 -0700

    Knight Tests Passes

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -1,10 +1,57 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class KnightMovesCalculator implements PieceMovesCalculator{
+    ArrayList<ChessMove> possibleMoves = new ArrayList<>();
+    int[][] knightMoves = {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+
     @Override
-    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor pieceColor) {
-        return null;
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition oldPosition, ChessGame.TeamColor pieceColor) {
+
+        int currRow = oldPosition.getRow();
+        int currCol = oldPosition.getColumn();
+
+        for (int[] move: knightMoves) {
+
+            int moveRow = currRow + move[0];
+            int moveCol = currCol + move[1];
+
+            ChessPosition newPosition = new ChessPosition(moveRow, moveCol);
+
+            if (isValidNoCheck(board, oldPosition, newPosition, pieceColor)) {
+                ChessMove currMove = new ChessMove(oldPosition, newPosition, null);
+                possibleMoves.add(currMove);
+            }
+        }
+
+        return possibleMoves;
+    }
+
+
+    private boolean isValidNoCheck(ChessBoard board, ChessPosition oldPosition, ChessPosition newPosition, ChessGame.TeamColor pieceColor) {
+        // Checks if in Bounds
+        if (board.inBounds(newPosition)) {
+
+            ChessPiece otherPiece = board.getPiece(newPosition);
+
+            // If no piece is there, add
+            if (otherPiece == null) {
+                return true;
+            }
+            //If piece is friendly, can't move. If no, list as move then can't move.
+            else {
+                if (otherPiece.getTeamColor() == pieceColor) {
+                    return false;
+                }
+                else {
+                    ChessMove currMove = new ChessMove(oldPosition, newPosition, null);
+                    possibleMoves.add(currMove);
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 }
