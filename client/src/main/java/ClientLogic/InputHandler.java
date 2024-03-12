d2243b31ed101201754321d43e0e77de5bdb96b9
KaidenWilliams <kman16409@gmail.com>
2024-03-11 22:41:14 -0600

    Making files, devising Plans

---
+++ client/src/main/java/ClientLogic/InputHandler.java
@@ -0,0 +1,36 @@
+package ClientLogic;
+
+import java.util.Scanner;
+
+public class InputHandler {
+
+    // Has scanner, handles input
+    // Constantly taking input, passes it to client
+    // Initialized Client
+    // Kills/stops looping upon recieving Quit message
+
+    // Calls client
+
+
+//    public void run() {
+//        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
+//        System.out.print(client.help());
+//
+//        Scanner scanner = new Scanner(System.in);
+//        var result = "";
+//        while (!result.equals("quit")) {
+//            printPrompt();
+//            String line = scanner.nextLine();
+//
+//            try {
+//                result = client.eval(line);
+//                System.out.print(BLUE + result);
+//            } catch (Throwable e) {
+//                var msg = e.toString();
+//                System.out.print(msg);
+//            }
+//        }
+//        System.out.println();
+//    }
+
+}
---
+++ client/src/main/java/ui/BoardBuilder.java
@@ -0,0 +1,9 @@
+package ui;
+
+public class BoardBuilder {
+
+//     This will make string representation of board
+//     class that encapsulates logic for how chess board look
+
+
+}
